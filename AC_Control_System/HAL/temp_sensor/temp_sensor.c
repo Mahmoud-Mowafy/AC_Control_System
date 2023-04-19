1da31f3163d948d67ea852471dcb54de308ecf3b
Mahmoud Mowafey <mahmsa37@gmail.com>
2023-04-19 17:53:51 +0200

    implement temperature get value function

--- AC_Control_System/HAL/temp_sensor/temp_sensor.c
+++ AC_Control_System/HAL/temp_sensor/temp_sensor.c
@@ -6,11 +6,21 @@
  */ 
 #include "temp_sensor.h"
 
-EN_tempsensorEror_T TEMPSENSOR_init(void)
+/*define global variable*/
+u16 u16_g_adcGetValue = 0;
+EN_tempSensorEror_T TEMPSENSOR_init(void)
 {
-	
+	u8 u8_l_errorState = DIO_init( TEMPSENSOR_CHANNEL, TEMPSENSOR_PORT, DIO_IN);
+	if ( u8_l_errorState == TEMPSENSOR_OK )
+	{
+		return STD_OK;
+	}
+	else
+	{
+		return STD_NOK;
+	}
 }
-EN_tempsensorEror_T TEMPSENSOR_updateValue(void)
+EN_tempSensorEror_T TEMPSENSOR_updateValue(void)
 {
 	u8 u8_l_errorState = ADC_startConversion(TEMPSENSOR_CHANNEL);
 	if ( u8_l_errorState == TEMPSENSOR_OK )
@@ -21,4 +31,18 @@ EN_tempsensorEror_T TEMPSENSOR_updateValue(void)
 	{
 		return STD_NOK;
 	}
+}
+
+EN_tempSensorEror_T TEMPSENSOR_getValue(u16 u16_a_adcValue)
+{
+	u8 u8_l_errorState = ADC_getDigitalValue( ADC_U8_CC_INT_MODE, &u16_a_adcValue );
+	if ( u8_l_errorState == TEMPSENSOR_OK )
+	{
+		u16_g_adcGetValue = *u16_a_adcValue;
+		return STD_OK;
+	}
+	else
+	{
+		return STD_NOK;
+	}	
 }
