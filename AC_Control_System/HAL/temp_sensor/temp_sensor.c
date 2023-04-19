0df3b0d23aa4cefadd63423a90bd75b5159f354f
Mahmoud Mowafey <mahmsa37@gmail.com>
2023-04-19 17:12:00 +0200

    implement the temperature update function

--- AC_Control_System/HAL/temp_sensor/temp_sensor.c
+++ AC_Control_System/HAL/temp_sensor/temp_sensor.c
@@ -4,3 +4,21 @@
  * Created: 4/19/2023 2:10:59 AM
  *  Author: Mahmoud
  */ 
+#include "temp_sensor.h"
+
+EN_tempsensorEror_T TEMPSENSOR_init(void)
+{
+	
+}
+EN_tempsensorEror_T TEMPSENSOR_updateValue(void)
+{
+	u8 u8_l_errorState = ADC_startConversion(TEMPSENSOR_CHANNEL);
+	if ( u8_l_errorState == TEMPSENSOR_OK )
+	{
+		return STD_OK;
+	}
+	else 
+	{
+		return STD_NOK;
+	}
+}
